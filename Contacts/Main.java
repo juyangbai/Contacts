0a5ec36e566c82ad77eef53beadab2794b3af986
Juyang Bai <80396058+juyangbai@users.noreply.github.com>
2021-03-31 16:50:45 +0800

    Create Main.java

---
+++ Contacts/Main.java
@@ -0,0 +1,36 @@
+package JuyangBai.Java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Contacts contacts = new Contacts();
+
+        Contact contact0 = new Contact("John","17816118582","78599","5789","Friends");
+        Contact contact1 = new Contact("Alice","18743242290","69533","4312","Family");
+        Contact contact2 = new Contact("Mike","13903442390","49320","9043","Partner");
+
+        contacts.add(contact0);
+        contacts.add(contact1);
+        contacts.add(contact2);
+        contacts.print();
+        System.out.println();
+
+//        contacts.remove(contact0);
+//        contacts.print();
+//        System.out.println();
+
+        contacts.getContact(0).print();
+        System.out.println();
+
+        System.out.println(contacts.find(contact1));
+
+        contacts.sort();
+        contacts.print();
+        System.out.println();
+
+        contacts.sortByGroup();
+        contacts.print();
+    }
+}
