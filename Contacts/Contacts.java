9b95cf38c0ec4df43ec51ed4836afb310f502eda
juyangbai <juyangbai@gmail.com>
2021-03-31 17:42:18 +0800

    Add user input

--- Contacts/Contacts.java
+++ Contacts/Contacts.java
@@ -1,9 +1,35 @@
 package JuyangBai.Java;
 
+```java
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Scanner;
+
+/*
+Class Contact is a definition for a contact person in your mobile phone book.
+Class Contacts uses an ArrayList<Contact> object to store your contacts to form your actual phone book.
+The class skeletons are given as follows (An IntelliJ IDEA project contained in Contacts.zip of the source code can be downloaded along with the exercise).
+Complete their definitions and fulfill the bodies of methods according to the description given in the comments.
+Test your implementation in method main. You may add any attributes and methods as needed based upon your own understanding of the problem.
+ */
+
+
+/*
+Errors
+E:\Java\jdk-15\bin\java.exe "-javaagent:E:\IntelliJ IDEA\IntelliJ IDEA Educational Edition 2020.3.3\lib\idea_rt.jar=55989:E:\IntelliJ IDEA\IntelliJ IDEA Educational Edition 2020.3.3\bin" -Dfile.encoding=UTF-8 -classpath D:\GPA\Java\Document\Contacts\out\production\Contacts edu.zstu.Jiang.Main
+Exception in thread "main" java.lang.NullPointerException: Cannot invoke "java.util.ArrayList.add(Object)" because "this.book" is null
+   at edu.zstu.Jiang.Contacts.add(Contacts.java:51)
+   at edu.zstu.Jiang.Main.main(Main.java:15)
+
+Process finished with exit code
+
+Solutions:
+ArrayList<Contact> book
+Add "= new ArrayList<>()"
+The method of initialize an arraylist is: ArrayList<T> arraylist = new Arraylist<>();
+ */
 
 class Contact
 {
@@ -12,6 +38,12 @@ class Contact
     String homePhoneNumber;
     String officePhoneNumber;
     String group;
+
+    Contact()
+    {
+
+    }
+
     Contact(String name,String cellPhoneNumber,String homePhoneNumber,String officePhoneNumber,String group)
     {
         //fill in your code here
@@ -21,6 +53,18 @@ class Contact
         this.officePhoneNumber = officePhoneNumber;
         this.group = group;
     }
+
+    void input()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        this.name = scanner.next();
+        this.cellPhoneNumber = scanner.next();
+        this.homePhoneNumber = scanner.next();
+        this.officePhoneNumber = scanner.next();
+        this.group = scanner.next();
+    }
+
     void print()//print out the attributes
     {
         //fill in your code here
@@ -34,7 +78,7 @@ public class Contacts {
 
     Contacts()
     {
-        /* fill in code here */
+        // fill in code here
     }
 
     void add(Contact contact)
@@ -123,3 +167,5 @@ public class Contacts {
     }
 
 }
+```
+
--- Contacts/Main.java
+++ Contacts/Main.java
@@ -1,15 +1,47 @@
+```java
 package JuyangBai.Java;
+
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-
+   // write your code here
         Contacts contacts = new Contacts();
 
-        Contact contact0 = new Contact("John","17816118582","78599","5789","Friends");
-        Contact contact1 = new Contact("Alice","18743242290","69533","4312","Family");
-        Contact contact2 = new Contact("Mike","13903442390","49320","9043","Partner");
+//        Contact contact0 = new Contact("John","17816118582","78599","5789","Friends");
+//        Contact contact1 = new Contact("Alice","18743242290","69533","4312","Family");
+//        Contact contact2 = new Contact("Mike","13903442390","49320","9043","Partner");
+//
+//        contacts.add(contact0);
+//        contacts.add(contact1);
+//        contacts.add(contact2);
+//        contacts.print();
+//        System.out.println();
+//
+////        contacts.remove(contact0);
+////        contacts.print();
+////        System.out.println();
+//
+//        contacts.getContact(0).print();
+//        System.out.println();
+//
+//        System.out.println(contacts.find(contact1));
+//
+//        contacts.sort();
+//        contacts.print();
+//        System.out.println();
+//
+//        contacts.sortByGroup();
+//        contacts.print();
+
+        Contact contact0 = new Contact();
+        Contact contact1 = new Contact();
+        Contact contact2 = new Contact();
+
+        System.out.println("PLease enter your contact information in the following format: name cellPhoneNumber homePhoneNumber officePhoneNumber group");
+        contact0.input();
+        contact1.input();
+        contact2.input();
 
         contacts.add(contact0);
         contacts.add(contact1);
@@ -17,10 +49,11 @@ public class Main {
         contacts.print();
         System.out.println();
 
-//        contacts.remove(contact0);
-//        contacts.print();
-//        System.out.println();
+        contacts.remove(contact0);
+        contacts.print();
+        System.out.println();
 
+        contacts.add(contact0);
         contacts.getContact(0).print();
         System.out.println();
 
@@ -34,3 +67,5 @@ public class Main {
         contacts.print();
     }
 }
+```
+
